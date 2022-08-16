84b8cad38083f37bf161793efcea32b51d7c6a24
AntonH <anton160687@gmail.com>
2022-08-16 18:13:59 +0300

    add ДЗ-3

--- Задача 13/Program.cs
+++ Задача 13/Program.cs
@@ -4,9 +4,11 @@ int num1 = num % 100;
 int num2 = num % 1000;
 int result = (num2 - num1) / 100;
 int num3 = num / 100;
-if (num3 != 0) {
-Console.WriteLine($"{result}");
+if (num3 != 0)
+{
+    Console.WriteLine($"{result}");
 }
-else {
-   Console.WriteLine("третьей цифры нет");
+else
+{
+    Console.WriteLine("третьей цифры нет");
 }
---
+++ Семинар 3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите номер четверти:");
+int num = int.Parse(Console.ReadLine());
+if (num == 1) {
+    Console.WriteLine("x > 0 и y > 0");
+}
+if (num == 2) {
+    Console.WriteLine("x < 0 и y > 0");
+}
+if (num == 3) {
+    Console.WriteLine("x < 0 и y < 0");
+}
+if (num == 4) {
+    Console.WriteLine("x > 0 и y < 0");
+}
+switch (chetvert) {
+    case 1:
+    Console.WriteLine("x > 0 и y < 0");
+}
